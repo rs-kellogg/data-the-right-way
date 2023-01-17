5ce424460784198b545d61cfedc26d3618e34576
schorndorfer <wkt@northwestern.edu>
2023-01-17 00:01:29 -0600

    updates

--- comscore-project/run_data_pipeline.sh
+++ comscore-project/run_data_pipeline.sh
@@ -7,3 +7,4 @@ fi
 
 python main.py ${1} comscore-filter.jinja 2019-01-01 2019-01-03 --dir ./output/logs
 aws s3 sync s3://kdc-comscore/parquet-extracts ./output/parquet --profile ${1}
+pytest tests
