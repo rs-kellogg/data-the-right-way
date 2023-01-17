de7d480f20f3765682e81523cf3f6742c576c5c8
schorndorfer <wkt@northwestern.edu>
2023-01-16 23:52:00 -0600

    updates

--- comscore-project/run_data_pipeline.sh
+++ comscore-project/run_data_pipeline.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'ERROR: provide the AWS profile name to use'
+    exit 1
+fi
+
 python main.py ${1} comscore-filter.jinja 2019-01-01 2019-01-03 --dir ./output/logs
 aws s3 sync s3://kdc-comscore/parquet-extracts ./output/parquet --profile ${1}
